0c75992016cecd763b1388b681f71e2d0921bb0f
mindslash79 <mindslash79@gmail.com>
2026-08-29 23:39:39 -0400

    Add Allstory game route

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/game",
+        destination: "/game/index.html",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
